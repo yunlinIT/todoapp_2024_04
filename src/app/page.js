8d0f7df45d999604ebb960c50ec49d2bb0489ba8
yunlinIT <yunlinit@gmail.com>
2024-04-02 11:21:30 +0900

    MUI button 컴포넌트, React Icons

--- src/app/page.js
+++ src/app/page.js
@@ -4,12 +4,40 @@ import * as React from 'react';
 import { ThemeProvider } from '@mui/material/styles';
 import { Button, Box, AppBar, Toolbar } from '@mui/material';
 import theme from './theme';
+import { MdDeleteForever } from 'react-icons/md';
 
 export default function App() {
   return (
     <>
       <ThemeProvider theme={theme}>
-        <Button variant="contained">버튼</Button>
+        <div className="tw-flex tw-items-center tw-gap-x-3">
+          <Button variant="text" endIcon={<MdDeleteForever />}>
+            Text
+          </Button>
+          <Button
+            variant="contained"
+            startIcon={<MdDeleteForever />}
+            onClick={() => confirm('삭제할거야?')}>
+            삭제
+          </Button>
+          <Button variant="outlined">Outlined</Button>
+        </div>
+
+        <div className="tw-flex tw-items-center tw-gap-x-3 tw-mt-3">
+          <Button
+            variant="text"
+            onClick={() => {
+              alert('버튼 클릭됨');
+            }}>
+            Text
+          </Button>
+          <Button variant="contained" disabled>
+            Contained
+          </Button>
+          <Button variant="outlined" href="sub/">
+            sub로 이동
+          </Button>
+        </div>
       </ThemeProvider>
     </>
   );
