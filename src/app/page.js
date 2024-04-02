21e3ad0d66bcf72e9e4a0ef6d0f973eb9d34cf3d
yunlinIT <yunlinit@gmail.com>
2024-04-02 15:17:10 +0900

    MUI Drawer 컴포넌트

--- src/app/page.js
+++ src/app/page.js
@@ -2,9 +2,10 @@
 
 import * as React from 'react';
 import { ThemeProvider } from '@mui/material/styles';
-import { Button, AppBar, Toolbar, Backdrop, CircularProgress } from '@mui/material';
+import { Button, AppBar, Toolbar, Drawer, List, ListItemButton } from '@mui/material';
 import theme from './theme';
 import { FaBars } from 'react-icons/fa';
+import Link from 'next/link';
 
 export default function App() {
   const [open, setOpen] = React.useState(false);
@@ -15,7 +16,7 @@ export default function App() {
         <AppBar position="fixed">
           <Toolbar>
             <div className="tw-flex-1">
-              <FaBars className="tw-cursor-pointer" />
+              <FaBars onClick={() => setOpen(true)} className="tw-cursor-pointer" />
             </div>
             <div className="logo-box">
               <a href="/" className="tw-font-bold">
@@ -32,13 +33,16 @@ export default function App() {
           section
         </section>
       </ThemeProvider>
-      <Button onClick={() => setOpen(true)}>Show backdrop</Button>
-      <Backdrop
-        sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
-        open={open}
-        onClick={() => setOpen(false)}>
-        <CircularProgress color="inherit" />
-      </Backdrop>
+      <Button onClick={() => setOpen(true)}>show drawer</Button>
+      <Drawer anchor="left" open={open} onClose={() => setOpen(false)}>
+        <List>
+          <ListItemButton>
+            <Link href="/write">글 쓰기</Link>
+          </ListItemButton>
+          <ListItemButton>사과</ListItemButton>
+          <ListItemButton>바나나</ListItemButton>
+        </List>
+      </Drawer>
     </>
   );
 }
