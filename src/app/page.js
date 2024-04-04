f7f2348ca59081ec8e8eb9b0e200aaa74aa628ff
yunlinIT <yunlinit@gmail.com>
2024-04-04 13:39:15 +0900

    Drawer 관련 가독성 개선

--- src/app/page.js
+++ src/app/page.js
@@ -89,7 +89,7 @@ const NewTodoForm = ({ todosState }) => {
   );
 };
 
-const TodoListItem = ({ todo, index, setOptionDrawerTodoId }) => {
+const TodoListItem = ({ todo, index, openDrawer }) => {
   return (
     <>
       <li key={todo.id}>
@@ -121,7 +121,7 @@ const TodoListItem = ({ todo, index, setOptionDrawerTodoId }) => {
             </div>
             <Button
               onClick={() => {
-                setOptionDrawerTodoId(todo.id);
+                openDrawer(todo.id);
               }}
               className="tw-flex-shrink-0 tw-rounded-[0_10px_10px_0]"
               color="inherit">
@@ -136,14 +136,15 @@ const TodoListItem = ({ todo, index, setOptionDrawerTodoId }) => {
 
 const TodoList = ({ todosState }) => {
   const [optionDrawerTodoId, setOptionDrawerTodoId] = React.useState(null);
+
+  const drawerOpened = React.useMemo(() => optionDrawerTodoId !== null, [optionDrawerTodoId]);
+
+  const openDrawer = (id) => setOptionDrawerTodoId(id);
+  const closeDrawer = () => setOptionDrawerTodoId(null);
+
   return (
     <>
-      <Drawer
-        anchor="bottom"
-        open={optionDrawerTodoId !== null}
-        onClose={() => {
-          setOptionDrawerTodoId(null);
-        }}>
+      <Drawer anchor="bottom" open={drawerOpened} onClose={closeDrawer}>
         <div className="tw-p-[30px] tw-flex tw-gap-x-[5px]">
           {optionDrawerTodoId}번 todo에 대한 옵션 Drawer
           <div>수정</div>
@@ -154,12 +155,7 @@ const TodoList = ({ todosState }) => {
       <nav>
         <ul>
           {todosState.todos.map((todo, index) => (
-            <TodoListItem
-              key={todo.id}
-              todo={todo}
-              index={index}
-              setOptionDrawerTodoId={setOptionDrawerTodoId}
-            />
+            <TodoListItem key={todo.id} todo={todo} index={index} openDrawer={openDrawer} />
           ))}
         </ul>
       </nav>
